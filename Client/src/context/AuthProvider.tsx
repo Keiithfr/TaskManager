de674f8a63f6d9b92b432de25490166b4a4dd5c8
keiith9 <keithinthebooth@gmail.com>
2026-06-30 10:00:19 +0300

    update context

--- Client/src/context/AuthProvider.tsx
+++ Client/src/context/AuthProvider.tsx
@@ -1,34 +1,73 @@
-import { ReactNode, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import type { ReactNode } from "react";
 import { AuthContext } from "./AuthContext";
-import { User } from "../types/auth";
+import type { User } from "../types/auth";
 
 interface Props {
     children: ReactNode;
 }
 
-export default AuthProvider = ({ children }: Props) => {
+const AuthProvider = ({ children }: Props) => {
     const [user, setUser] = useState<User | null>(null);
     const [loading, setLoading] = useState(true);
 
-    const login=()=>{
-        
-    }
-    const logout=()=>{
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                const res = await fetch(
+                    `${import.meta.env.VITE_API_URL}/me`, {
+                    credentials: "include"
+                }
+                );
+                if (res.ok) {
+                    const data = await res.json();
+                    setUser(data);
+                }
+            } catch (err) {
+                if (err instanceof Error) {
+                    console.log(err)
+                }
+            } finally {
+                setLoading(false)
+            }
+        };
+
+        fetchUser();
+
+    }, []);
+
+    const login = (userData: User) => {
+        setUser(userData)
 
     }
+    const logout = async () => {
+        try {
+            await fetch(
+                `${import.meta.env.VITE_API_URL}/logout`, {
+                method: "POST",
+                credentials: "include"
+            }
+            );
+            setUser(null);
+        } catch (err) {
+            if (err instanceof Error) {
+                console.log(err)
+            }
+        }
 
-    useEffect(()=>{
+    }
 
-    },[])
 
-    return(
+
+    return (
         <AuthContext.Provider value={{
-            user,loading,login,logout
+            user, loading, login, logout
         }}>
             {children}
         </AuthContext.Provider>
     )
 
-    }
+};
+export default AuthProvider
 
 
--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,13 +1,17 @@
-
+import AuthProvider from './context/AuthProvider.tsx'
 import {BrowserRouter} from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <AuthProvider>
+      <BrowserRouter>
    <App />
    </BrowserRouter>
+
+  </AuthProvider>
+
    
   
 )
