eb60981c0a4fb7eb3115544d6c3d9b7f8c1f2562
keiith9 <keithinthebooth@gmail.com>
2026-08-21 11:26:43 +0300

    add dashboard

---
+++ Client/src/Pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,20 @@
+import styles from './Dashboard.module.css'
+
+const Dashboard = () => {
+
+
+    return (
+        <>
+            <div className={styles.top}>
+                <h1>DASHBOARD</h1>
+            </div>
+        </>
+
+
+
+
+    )
+
+}
+
+export default Dashboard;
